9ae11370860545b58ec82fc260f53337c15b84b6
NicoGonzalez6 <Nicolasgonzalez619@hotmail.com>
2024-01-17 20:33:39 -0300

    patch fix

--- dist/preset/screen.js
+++
@@ -1,8 +0,0 @@
-export const screens = {
-  xs: { min: "0px" },
-  sm: { min: "640px" },
-  md: { min: "768px" },
-  lg: { min: "1024px" },
-  xl: { min: "1280px" },
-  "2xl": { min: "1440px" },
-};
